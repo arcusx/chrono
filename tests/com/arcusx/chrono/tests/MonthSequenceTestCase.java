a7e07ed39334daab2eedec169b9fa7158bc2f9db
conni <c.buschka@arcusx.com>
2005-02-22 17:09:19 +0000

    added tests for SimpleMonthSequenceFormat

--- tests/com/arcusx/chrono/tests/MonthSequenceTestCase.java
+++ tests/com/arcusx/chrono/tests/MonthSequenceTestCase.java
@@ -95,4 +95,48 @@ public class MonthSequenceTestCase extends TestCase
 		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 5), iter.next());
 		assertTrue(!iter.hasNext());
 	}
+	
+	public void testParseOneMonth() throws Exception
+	{
+		MonthSequence seq = SimpleMonthSequenceFormat.INSTANCE.parse("2004/01");
+		Iterator iter = seq.getMonthsParts().iterator();
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 1), iter.next());
+		assertTrue(!iter.hasNext());
+	}
+
+	public void testParseManyMonth() throws Exception
+	{
+		MonthSequence seq = SimpleMonthSequenceFormat.INSTANCE.parse("2004/01,2004/02,2004/04,2004/05");
+		Iterator iter = seq.getMonthsParts().iterator();
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 2), iter.next());
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.APRIL), 2), iter.next());
+		assertTrue(!iter.hasNext());
+	}
+
+	public void testParseOneMonths() throws Exception
+	{
+		MonthSequence seq = SimpleMonthSequenceFormat.INSTANCE.parse("2004/01-2004/02");
+		Iterator iter = seq.getMonthsParts().iterator();
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 2), iter.next());
+		assertTrue(!iter.hasNext());
+	}
+
+	public void testParseManyMonths() throws Exception
+	{
+		MonthSequence seq = SimpleMonthSequenceFormat.INSTANCE.parse("2004/01-2004/02,2004/04-2004/05");
+		Iterator iter = seq.getMonthsParts().iterator();
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 2), iter.next());
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.APRIL), 2), iter.next());
+		assertTrue(!iter.hasNext());
+	}
+
+	public void testParseManyMonthAndMonthsMixed() throws Exception
+	{
+		MonthSequence seq = SimpleMonthSequenceFormat.INSTANCE.parse("2004/01-2004/02,2004/04,2004/05,2004/06,2004/08-2004/12");
+		Iterator iter = seq.getMonthsParts().iterator();
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.JANUARY), 2), iter.next());
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.APRIL), 3), iter.next());
+		assertEquals((Object) new Months(new Month(2004, MonthOfYear.AUGUST), 5), iter.next());
+		assertTrue(!iter.hasNext());
+	}
 }
